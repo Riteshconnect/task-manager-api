c0d1bedafc7eaec5a5684e099d0de9eb6cef9a81
Ritesh Kumar <riteshkumar12040211@gmail.com>
2026-02-18 10:46:09 +0530

    MongoDB connection and User model created

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
+const connectDB = require("./src/config/db")
 
-const PORT = process.env.PORT || 5000;
+connectDB();
+
+const PORT = process.env.PORT;
 
 app.listen(PORT,()=>{
-    console.log("Sevrer running on PORT");
-});
+    console.log(`Sevrer running on PORT ${PORT}`);
+})
