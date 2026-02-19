63889dae4e37b97975d33dbe6fc00f1bb0707711
Ritesh Kumar <riteshkumar12040211@gmail.com>
2026-02-19 11:00:36 +0530

    Implemented full Task CRUD APIs with authentication

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -30,8 +30,107 @@ const createTask = async (req, res) => {
     }
 
 };
+// Get all tasks of logged-in user
+const getTasks = async (req, res) => {
+
+    try {
+
+        const tasks = await Task.find({
+            user: req.userId
+        });
+
+        res.status(200).json({
+            tasks
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+};
+
+// Update Task
+const updateTask = async (req, res) => {
+
+    try {
+
+        const taskId = req.params.id;
+
+        const { title, description, completed } = req.body;
+
+        const task = await Task.findOneAndUpdate(
+
+            { _id: taskId, user: req.userId }, // ensures user owns task
+
+            { title, description, completed },
+
+            { new: true }
+        );
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Task updated successfully",
+            task
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+};
+
+// Delete Task
+const deleteTask = async (req, res) => {
+
+    try {
+
+        const taskId = req.params.id;
+
+        const task = await Task.findOneAndDelete({
+
+            _id: taskId,
+            user: req.userId
+
+        });
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Task deleted successfully"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+};
+
 
 
 module.exports = {
-    createTask
+    createTask,
+    getTasks,
+    updateTask,
+    deleteTask
 };
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -5,12 +5,18 @@ const router = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 
 const {
-    createTask
+    createTask,
+    getTasks,
+    updateTask,
+    deleteTask
 } = require("../controllers/task.controller");
 
 
 // create task route
 router.post("/", authMiddleware, createTask);
+router.get("/",authMiddleware,getTasks);
+router.put("/:id",authMiddleware,updateTask)
+router.delete("/:id",authMiddleware,deleteTask)
 
 
 module.exports = router;
